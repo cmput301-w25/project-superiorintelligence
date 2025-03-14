ee64bdf8feddc873d34100dfcb5978b6ab71c91c
Michael <mppattison@shaw.ca>
2025-03-13 22:24:00 -0600

    signout button functionality implemented

--- code/app/src/main/java/com/example/superior_intelligence/ProfileActivity.java
+++ code/app/src/main/java/com/example/superior_intelligence/ProfileActivity.java
@@ -3,8 +3,12 @@ package com.example.superior_intelligence;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 public class ProfileActivity extends AppCompatActivity {
     @Override
@@ -20,6 +24,16 @@ public class ProfileActivity extends AppCompatActivity {
                 Intent intent = new Intent(ProfileActivity.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        // signout button
+        LinearLayout signoutButton = findViewById(R.id.signout);
+        signoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfileActivity.this, LoginPageActivity.class);
+                startActivity(intent);
             }
         });
     }
