c9c89b3a8ff51e5ebe518bb584e9d865f98c71b9
arahmad-ualberta <169095788+arahmad-ualberta@users.noreply.github.com>
2025-03-18 03:32:45 -0600

    reimplemented imports

--- code/app/src/main/java/com/example/superior_intelligence/EventAdapter.java
+++ code/app/src/main/java/com/example/superior_intelligence/EventAdapter.java
@@ -1,10 +1,15 @@
 package com.example.superior_intelligence;
+import com.example.superior_intelligence.Photobase;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.os.Handler;
 import android.content.Context;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
