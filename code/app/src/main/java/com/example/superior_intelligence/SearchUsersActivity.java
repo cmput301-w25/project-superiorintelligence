554edf42db8e0f3fe4c9257d1bf0c371c306739b
Michael <mppattison@shaw.ca>
2025-03-20 15:59:30 -0600

    User Search page implemented, no functionality except back arrow, button to enter user search page implemented on profile page

--- code/app/src/main/java/com/example/superior_intelligence/SearchUsersActivity.java
+++ code/app/src/main/java/com/example/superior_intelligence/SearchUsersActivity.java
@@ -1,4 +1,27 @@
 package com.example.superior_intelligence;
 
-public class SearchUsersActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchUsersActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_users);
+
+        // Back button to returns to HomeActivity
+        ImageButton backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish(); // Closes the current activity and returns to the previous screen
+            }
+        });
+
+
+    }
 }
