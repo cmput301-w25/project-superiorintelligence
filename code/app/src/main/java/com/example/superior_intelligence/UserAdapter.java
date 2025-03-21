695457abda186bbaada2a7ec46d399839ca3fd81
Michael <mppattison@shaw.ca>
2025-03-20 18:00:39 -0600

    completed

--- code/app/src/main/java/com/example/superior_intelligence/UserAdapter.java
+++ code/app/src/main/java/com/example/superior_intelligence/UserAdapter.java
@@ -1,3 +1,8 @@
+/**
+ * Adapter class for displaying a list of users in a RecyclerView.
+ * This class binds data from a list of {@link HelperClass} objects to the UI.
+ */
+
 package com.example.superior_intelligence;
 
 import android.view.LayoutInflater;
@@ -11,10 +16,22 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
 
     private List<HelperClass> userList;
 
+    /**
+     * Constructs a new {@code UserAdapter} with the specified list of users.
+     *
+     * @param userList the list of {@link HelperClass} objects to be displayed.
+     */
     public UserAdapter(List<HelperClass> userList) {
         this.userList = userList;
     }
 
+    /**
+     * Called when the RecyclerView needs a new {@link UserViewHolder} to represent an item.
+     *
+     * @param parent   the parent ViewGroup.
+     * @param viewType the view type of the new View.
+     * @return a new {@code UserViewHolder} that holds a View of the given view type.
+     */
     @Override
     public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
@@ -22,6 +39,12 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         return new UserViewHolder(view);
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     *
+     * @param holder   the {@code UserViewHolder} to bind the data to.
+     * @param position the position of the data item within the dataset.
+     */
     @Override
     public void onBindViewHolder(UserViewHolder holder, int position) {
         HelperClass user = userList.get(position);
@@ -29,14 +52,27 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder
         holder.usernameTextView.setText(user.getUsername());
     }
 
+    /**
+     * Returns the total number of items in the dataset.
+     *
+     * @return the size of the dataset.
+     */
     @Override
     public int getItemCount() {
         return userList.size();
     }
 
+    /**
+     * ViewHolder class for holding user item views.
+     */
     public static class UserViewHolder extends RecyclerView.ViewHolder {
         TextView nameTextView, usernameTextView;
 
+        /**
+         * Constructs a new {@code UserViewHolder} with the specified view.
+         *
+         * @param itemView the view representing a single item in the list.
+         */
         public UserViewHolder(View itemView) {
             super(itemView);
             nameTextView = itemView.findViewById(R.id.user_name);
