6102a8ae39fa812bf2643b5777678d9a8ab6ce59
Kennedy Barber <116937764+KennedyBarber@users.noreply.github.com>
2025-03-22 15:55:03 -0600

    tests for comments user stories and javadoc

--- code/app/src/main/java/com/example/superior_intelligence/Comment.java
+++ code/app/src/main/java/com/example/superior_intelligence/Comment.java
@@ -8,36 +8,69 @@ public class Comment implements Serializable {
     private String text;
     private String time;
 
+    /**
+     * Default constructor for serialization and Firebase operations.
+     */
     public Comment() {
 
     }
 
+    /**
+     * Constructs a new Comment instance with the specified username, text, and timestamp.
+     * @param username the username of the commenter
+     * @param text     the content of the comment
+     * @param time     the timestamp when the comment was created, formatted as a string
+     */
     public Comment(String username, String text, String time) {
         this.username = username;
         this.text = text;
         this.time = time;
     }
 
+    /**
+     * Retrieves the username of the commenter.
+     * @return the commenter's username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the username of the commenter.
+     * @param username the username to set
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Retrieves the content of the comment.
+     * @return the comment text
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * Sets the content of the comment.
+     * @param text the text content of the comment to set
+     */
     public void setText(String text) {
         this.text = text;
     }
 
+    /**
+     * Retrieves the timestamp of when the comment was created.
+     * @return the comment timestamp as a formatted string
+     */
     public String getTime() {
         return time;
     }
 
+    /**
+     * Sets the timestamp of the comment.
+     * @param time the timestamp of the comment creation to set, formatted as a string
+     */
     public void setTime(String time) {
         this.time = time;
     }
