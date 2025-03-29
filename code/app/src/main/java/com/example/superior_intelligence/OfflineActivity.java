2189869c29e092dfb31f21299ae3051cf1260216
Michael <mppattison@shaw.ca>
2025-03-29 16:04:36 -0600

    test cases must still be implemented

--- code/app/src/main/java/com/example/superior_intelligence/OfflineActivity.java
+++ code/app/src/main/java/com/example/superior_intelligence/OfflineActivity.java
@@ -1,44 +1,3 @@
-//package com.example.superior_intelligence;
-//
-///**
-// * Handle when user is disconnected from the network, save and restore when user gets the connection back
-// */
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//
-////code taken from: https://firebase.google.com/docs/database/android/offline-capabilities#java
-//public class OfflineActivity extends AppCompatActivity {
-//
-//    private static final String TAG = "OfflineActivity";
-//
-//    /**
-//     * enable disk persistence: app will write the data locally to the device so it can
-//     * maintain state while offline, even if the user or operating system restarts the app.
-//     */
-//    private void enablePersistence() {
-//        // [START rtdb_enable_persistence]
-//        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
-//        // [END rtdb_enable_persistence]
-//    }
-//
-//
-//    private void keepSynced() {
-//        // [START rtdb_keep_synced]
-//        DatabaseReference eventsRef = FirebaseDatabase.getInstance().getReference("Event");
-//        eventsRef.keepSynced(true);
-//        // [END rtdb_keep_synced]
-//
-//        // [START rtdb_undo_keep_synced]
-//        eventsRef.keepSynced(false);
-//        // [END rtdb_undo_keep_synced]
-//    }
-//
-//
-//}
-
 package com.example.superior_intelligence;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -50,6 +9,7 @@ import com.google.firebase.firestore.FirebaseFirestoreSettings;
  * Handles offline support for Firestore data persistence.
  * This activity enables Firestore's offline mode, allowing the app
  * to store and retrieve data even when the device is offline.
+ * issue: us 07.01.01, Link to sub issue:
  */
 
 public class OfflineActivity extends AppCompatActivity {
